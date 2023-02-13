c0014cfab98c6d2369fbf710d0cf71a290b19476
OlegTitov <titov-oe@mail.ru>
2023-02-13 21:08:19 +0300

    Создание Main и README.md

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        SalesManager version = new SalesManager(new int[]{100, 200, 300});
+        System.out.println(version.max());
+    }
+}
